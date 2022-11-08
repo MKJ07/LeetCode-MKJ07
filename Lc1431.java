85d4723e8d9a5c3e36441d86798ee6fcf98b24a1
MKJ07 <mahesh8479j@gmail.com>
2022-11-08 23:17:55 +0530

    commit

--- Lc1431.java
+++ Lc1431.java
@@ -1,15 +1,32 @@
-public class Lc1431 {
+import java.util.*;
+class Lc1431 {
+    static int largestElement(int[] arr) {
+        int largest_element =Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>largest_element)
+                largest_element=arr[i];
+        }
+        return largest_element;
+    }
     public static void main(String[] args) {
         int[] candies = {2,3,5,1,3}; int extraCandies = 3;
-        int flag=1;
+        int max=largestElement(candies);
+        Boolean[] b = new Boolean[candies.length];
         for(int i=0;i<candies.length;i++){
-            int s=candies[i]+extraCandies;
-            for(int j=1;j<candies.length;j++){
-               if(s>=candies[j]){
-
-                }
+            if((candies[i]+extraCandies)>=max){
+                b[i]=true;
+            }
+            else{
+                b[i]=false;
             }
         }
-        
+        ArrayList<Boolean> a1=new ArrayList<>();
+        for(int i=0;i<b.length;i++){
+            a1.add(b[i]);
+        }
+        for(Boolean i:a1){
+            System.out.print(i+" ");
+            
+        }  
     }
 }
