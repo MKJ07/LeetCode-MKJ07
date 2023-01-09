fd40cdccdfdb64d09da7b1e7917d8c4f3b08c0b5
MKJ07 <mahesh8479j@gmail.com>
2023-01-09 22:37:21 +0530

    Binary Tree Postorder Traversal

---
+++ Lc145.java
@@ -0,0 +1,56 @@
+import java.util.*;
+/*Question:- Binary Tree PostOrder Traversal
+ *Link:-https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+Input: root = []
+Output: []
+Input: root = [1]
+Output: [1]
+*/
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class Lc145 {
+    /* This is code you to know for the solution of the problem */
+    List<Integer> l=new ArrayList<>();
+    public void postOrder(TreeNode root){
+        if(root==null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        l.add(root.val);
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        postOrder(root);
+        return l;
+
+    }
+    /* Till here */
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        // List<Integer> list=new ArrayList<>();
+        Lc145 t=new Lc145();
+        System.out.println(t.postorderTraversal(createTree()));
+    }
+    static TreeNode createTree(){
+        TreeNode root=null;
+        int data=sc.nextInt();
+        if(data ==-1) return null;
+        root=new TreeNode(data);
+        root.left=createTree();
+        root.right=createTree();
+        return root;
+
+    }
+}
