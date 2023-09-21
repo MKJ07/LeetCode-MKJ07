16d06bda1ded9bdbe472a965482253ad591f70a8
MKJ07 <mahesh8479j@gmail.com>
2023-09-21 22:58:48 +0530

    POTD(21/09/23)

---
+++ Lc4.java
@@ -0,0 +1,47 @@
+public class Lc4 {
+    static int[] merge(int[] nums1,int[]nums2){
+        int i=0,j=0;
+        int n=nums1.length,m=nums2.length;
+        int[] res=new int[n+m];
+        int idx=0;
+        while(i<n && j<m){
+            if(nums1[i]<nums2[j]){
+                res[idx]=nums1[i];
+                idx++;
+                i++;
+            }
+            else{
+                res[idx]=nums2[j];
+                idx++;
+                j++;
+            }
+        }
+        while(i<n){
+            res[idx]=nums1[i];
+            i++;idx++;
+        }
+        while(j<m){
+            res[idx]=nums2[j];
+            j++;idx++;
+        }
+        return res;
+    }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] mergeArray=merge(nums1,nums2);
+        int n=mergeArray.length;
+        if(n%2==0){
+            int n1=mergeArray[n/2-1];
+            int n2=mergeArray[n/2];
+            return (double)(n1+n2)/2;
+        }
+        else{
+            return mergeArray[n/2];
+        }
+    }
+    public static void main(String[] args) {
+        int []arr1={1,2,4,5,8};
+        int []arr2={3,4,5,7};
+        Lc4 t=new Lc4();
+        System.out.println(t.findMedianSortedArrays(arr1, arr2));
+    }
+}
