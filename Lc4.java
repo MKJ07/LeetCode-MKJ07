0b22d781b46d5f696d1a4674df35e4a3e0d8a85a
MKJ07 <mahesh8479j@gmail.com>
2023-09-21 22:59:54 +0530

    POTD(21/09/23)

--- Lc4.java
+++ Lc4.java
@@ -1,3 +1,10 @@
+/*Question: 4. Median of Two Sorted Arrays.
+Link: https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+*/
 public class Lc4 {
     static int[] merge(int[] nums1,int[]nums2){
         int i=0,j=0;
