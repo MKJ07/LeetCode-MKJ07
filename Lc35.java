f157a72c98310f1085cfa8fedacfe246773017a9
MKJ07 <mahesh8479j@gmail.com>
2023-09-22 00:02:20 +0530

    Added optimal solution

--- Lc35.java
+++ Lc35.java
@@ -4,6 +4,29 @@ Input: nums = [1,3,5,6], target = 7
 Output: 4 */
 public class Lc35 {
     public int searchInsert(int[] nums, int target) {
+        /*This is using binary search using while loop and more clearly */
+        int n=nums.length;
+        int l=0,r=n-1;
+        int idx=0;
+        if(n==0|| nums[0]>=target){
+            return 0;
+        }
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]<target){
+                idx=mid;
+                l=mid+1;
+            }
+            else if(nums[mid]==target){
+                return mid;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return idx+1;
+
+        /*This is also using binary search but using for loop
         int min=0;
         int max=nums.length;
         int mid=0,f=0;
@@ -25,7 +48,7 @@ public class Lc35 {
                 return mid+1;
             }
         }
-        return mid;
+        return mid; */
 
         //Another way is given below
         // int c=0;
