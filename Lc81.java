02d9062a5dd76a606e9f42b0b9b397aac8d6c516
MKJ07 <mahesh8479j@gmail.com>
2023-10-02 20:05:17 +0530

    Added optimal approach

--- Lc81.java
+++ Lc81.java
@@ -9,16 +9,51 @@ Output: false
 */
 public class Lc81 {
     public static boolean search(int[] nums, int target) {
-        for(int i:nums){
-            if(i==target){
+        /*This is the optimal approach having TC:O(logN) for the best and average case. O(N/2) for the worst case
+         and SC:O(1) */
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
                 return true;
+            }
+
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+            } else if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
         return false;
-        
+
+        /*
+         * This is the brute force approach having TC:O(n) and SC:O(1)
+         * for(int i:nums){
+         * if(i==target){
+         * return true;
+         * }
+         * }
+         * return false;
+         */
+
     }
+
     public static void main(String[] args) {
-        int[] nums = {2,5,6,0,0,1,2};int target = 6;
+        int[] nums = { 2, 5, 6, 0, 0, 1, 2 };
+        int target = 6;
         System.out.println(search(nums, target));
     }
 }
