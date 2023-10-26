78165c8225c9f0248dd20fccb893eea5d9c80761
MKJ07 <mahesh8479j@gmail.com>
2023-10-26 22:28:45 +0530

    Questions on Arrays

--- Lc448.java
+++ Lc448.java
@@ -1,7 +1,28 @@
-import java.util.List;
+import java.util.*;
+/*Question: 448. Find All Numbers Disappeared in an Array.
+Link: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array
 
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+ */
 public class Lc448 {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        /*This is the optimal approach having TC:O(n) and SC:O(1) */
+        List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(nums[i]);
+            if (nums[num - 1] > 0) {
+                nums[num - 1] = -nums[num - 1];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        return result;
+
         /*This is the brute force approach having TC: O(n^2) and SC: O(1) 
         List<Integer> l=new ArrayList<>();
         for(int i=1;i<=nums.length;i++){
