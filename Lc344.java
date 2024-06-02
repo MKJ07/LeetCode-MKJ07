f44f2a53ea680c6be62b9bcce7022ed3e9883eae
MKJ07 <mahesh8479j@gmail.com>
2024-06-02 13:18:21 +0530

    POTD(02/06/24)

--- Lc344.java
+++ Lc344.java
@@ -1,10 +1,25 @@
-/*code link:-https://leetcode.com/problems/reverse-string/
+/*Question: 344. Reverse String.
+Link:-https://leetcode.com/problems/reverse-string/
+
 Input: s = ["h","e","l","l","o"]
 Output: ["o","l","l","e","h"]
+
 Input: s = ["H","a","n","n","a","h"]
-Output: ["h","a","n","n","a","H"] */
+Output: ["h","a","n","n","a","H"] 
+*/
 public class Lc344 {
-    public char[] reverseString(char[] s) {
+    public void reverseString(char[] s) {
+        /*This is the optimal solution having TC: O(n) and SC: O(1) {n: length of the array (using two pointer approach) } */
+        int high=s.length-1;
+        int low=0;
+        while(low<high){
+            char temp=s[low];
+            s[low]=s[high];
+            s[high]=temp;
+            high--;low++;
+        }
+
+        /*This is the brute force approach having TC: O(n) and SC: O(n) {n: length of the array } 
         char[] c=new char[s.length];
         int t=0;
         for(int i=s.length-1;i>=0;i--){
@@ -14,18 +29,14 @@ public class Lc344 {
         for(int i=0;i<c.length;i++){
             s[i]=c[i];
         }
-        return s;
+        return s;*/
     }
     public static void main(String[] args) {
         char[] s={'h','e','l','l','o'};
-        Lc344 l=new Lc344();
-        char[] s1=l.reverseString(s);
-        for(int i=0;i<s1.length;i++){
-            System.out.print(s1[i]);
-
+        Lc344 obj=new Lc344();
+        obj.reverseString(s);
+        for (char c : s) {
+            System.out.print(c+" ");
         }
-
-
-
     }
 }
